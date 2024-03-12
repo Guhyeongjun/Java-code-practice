db93afda5ea23b966d7813f1ae37d9dcd96c9cd5
Hyeongjun Gu <gujuns1987@naver.com>
2024-03-12 20:09:03 +0900

    add : scanner 함수 추가 - 사용자가 6개의 번호를 지정 - 사용자가 중복 선택하지 못하게 설정

--- LottoSystem.java
+++ LottoSystem.java
@@ -34,6 +34,39 @@ public class LottoSystem {
 				
 				lottoMachine[index] = 0; // 사용했다는 증명!
 			}
+
+			//system 로또 생성 검증
+		for(int i = 0; i <lottoBalls.length; i++) {
+			System.out.print(lottoBalls[i] + " ");
+		}
+		System.out.println("보너스 번호 : " + bonusNumber);
+		
+		//사용자가 선택!
+		count = 0;
+		Scanner sc = new Scanner(System.in);
+		//사용자 로또 저장 배열
+		int[] userLotto = new int[6];
+		
+		while(count < 6) {
+			System.out.println((count+1) + " 번째 로또 번호! : ");
+			int balls = sc.nextInt();
+			
+			//중복체크
+			for(int i = 0; i < count; i++) {
+				if(userLotto[i] == balls) {
+					System.out.println(balls + " 는 이미 존재하는 번호 !!");
+					balls = 0; // 중복되었다는 의미
+					break;
+				}
+			}
+			
+			//0은 중복의 의미
+			if(balls != 0) {
+				userLotto[count++] = balls;
+			}
+		}
+		
+		sc.close();
 		}
     }
 }
