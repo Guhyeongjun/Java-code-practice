d06ed6b4122b0df7fd05e7d33d719d5552d77984
HyeongJun GU <161324364+Guhyeongjun@users.noreply.github.com>
2024-04-12 16:40:57 +0900

    Upload : Java-code-practice 0412

---
+++ ProgrammerCode/src/level1/XtoN.java
@@ -0,0 +1,17 @@
+package level1;
+
+public class XtoN  {
+	public static void main(String[] args) {
+		class Solution {
+		    public long[] solution(int x, int n) {
+		        long[] answer = {};
+		        answer = new long[n];
+		        
+		        for(int i = 0; i < n; i++){
+		            answer[i] = Long.valueOf(x) * (i+1);
+		        }
+		        return answer;
+		    }
+		}
+	}
+}
